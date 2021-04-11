f11f8f71b774d63c42897c5c6ae5dbec8a3531a6
Leandro <leandrosf934@gmail.com>
2021-04-11 02:25:47 -0300

    update

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require( 'express')
 const router = require ('./src/routers/Routers.js')
+const nunnjucks = require('nunjucks')
 const app = express()
 
 app.use(express.static('./src/public/img'));
 app.use(express.static('./src/public/css'));
-app.use(express.static('./src/views'));
+//app.use(express.static('./src/public/js'));
+nunnjucks.configure('src/views', {
+    autoescape: true,
+    express:app,
+    watch: true
+})
 app.use(router)
 
 app.set('view engine', '.html'); 
