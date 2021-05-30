88baf0f0e0261ec601b0fdaa96e4d76765066194
Leandro <leandrosf934@gmail.com>
2021-05-30 12:55:35 -0300

    update

--- src/controller/Index.js
+++ src/controller/Index.js
@@ -5,16 +5,16 @@ exports.Index = async(req,res) => {
     res.render('index')
 }
 
+exports.DonatePicpay = async(req,res) => {
+  res.render('donatePicpay')
+}
+
 
 
 
 exports.ApiDDD = async(req, res) => {
 
   const ddd = Number(req.params.codigo)
-
-  
-    
-
     res.json((Localizar.localizar(ddd)))
 }
 
--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -4,5 +4,6 @@ var router = express.Router();
 
 router.get('/', ControllerIndex.Index)
 router.get(`/api/:codigo`, ControllerIndex.ApiDDD)
+router.get('/donatepicpay', ControllerIndex.DonatePicpay)
 
 module.exports = router
