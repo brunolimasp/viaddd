6db647a1c9bbb717b14773cd5396af0b498e514e
Leandro <leandrosf934@gmail.com>
2021-06-05 19:58:48 -0300

    update

--- src/controller/Index.js
+++ src/controller/Index.js
@@ -12,7 +12,7 @@ exports.ApiDDD = async(req, res) => {
     const ddd = Number(req.params.codigo)
 
     if(ddd != ddd.toString() ){
-      return res.json({erro:"Formato inválido!"})
+      return res.status(400).json({erro:"Formato inválido!"})
     }
     res.json((Localizar.localizar(ddd)))
 }
