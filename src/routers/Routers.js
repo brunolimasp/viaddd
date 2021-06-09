8d82de7a4c97cf10c5c745cfd8dc2dc6b5d228a4
Leandro <leandrosf934@gmail.com>
2021-06-09 19:40:09 -0300

    update

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -3,7 +3,7 @@ const ControllerIndex = require ('../controller/Index')
 var router = express.Router();
 
 router.get('/', ControllerIndex.Index)
-router.get(`/api/:codigo`, ControllerIndex.ApiDDD)
+router.get(`/api/ddd/:codigo`, ControllerIndex.ApiDDD)
 router.get('/politica', ControllerIndex.Politica)
 router.get('/sobre', ControllerIndex.Sobre)
 router.get('/historia', ControllerIndex.Historia)
